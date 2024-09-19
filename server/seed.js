d47883e311824e0f3703ce4de27c1404e84ebcf2
Ashir-Valjee <ashir@eco-motive.com>
2024-09-20 00:24:35 +0100

    added some code

--- server/seed.js
+++ server/seed.js
@@ -1,1 +1,27 @@
 // here you can CREATE your SQL queries and create table and dummy data
+// create  tables with SQL and insert dummy data for testing purposes
+// to seed a database is to manually insert data for testing purposes
+
+// write a SQL query to create a new table
+// we need to tell our server where to send the query
+// the query method needs the argument to be a string
+
+// i need to import db from server file
+// i use {} to destructure just one element
+
+import { db } from "./server.js";
+
+// if we already have a table with the same name we need to make sure
+// we dont create a new table with the same name
+
+db.query(`CREATE TABLE IF NOT EXISTS guests (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  date_of_visit DATE,
+  review TEXT
+);`);
+
+// i want to insert data into the pets table
+
+db.query(`INSERT INTO guests (name,date_of_visit,review)
+  VALUES ('James', '2024-04-30','great visit, wonderful service')`);
--- server/server.js
+++ server/server.js
@@ -1,18 +1,37 @@
 // access express,cors,pg,dotenv
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
 
 // initialise express
+const app = express();
 
 // configure dotenv
+dotenv.config();
 
 // tell my express app to use json
+app.use(express.json());
 
 //tell my express app to use cors
+app.use(cors());
 
 // i need to set up a PORT for my app to listen
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`server is running in PORT ${PORT}`);
+});
 
 // i need to set up my database pool using the connection string from the .env file
+const dbConnectionString = process.env.DATABASE_URL;
+export const db = new pg.Pool({
+  connectionString: dbConnectionString,
+});
 
 // need to set up a a root route
+app.get("/", (request, response) => {
+  response.json({ message: "you are looking at my root route. how rude" });
+});
 
 // you need two routes minimum
 
@@ -28,3 +47,18 @@
 
 // ============================
 // for this assignment the minimum you need is one table to store feedback
+
+app.get("/guests", async (request, response) => {
+  const query = await db.query(`SELECT * FROM guests`);
+
+  // we wrangle the query to get rows
+  response.json(query.rows);
+  console.log(query);
+});
+
+// whenever you need to specify data in your query do not write data directly. Use a parameter to replace
+// the data
+app.get("/guests2", async function (request, response) {
+  const query = await db.query(`SELECT * FROM guests WHERE name=$1`, ["James"]);
+  response.json(query.rows);
+});
